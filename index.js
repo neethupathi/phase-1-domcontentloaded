38361c1197edef955bdb6a2f04f17e1803b2d1e7
neethupath <pathnee222gmail.com>
2023-04-14 10:01:55 -0400

    dom

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+// The HTML for divA is now:
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    document.getElementById("text").textContent = "This is really cool!";
+  });
+
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
